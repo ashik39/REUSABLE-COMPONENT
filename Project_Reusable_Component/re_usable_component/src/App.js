5fbdd488bf4c9740819e23669e428fe6303814bb
saravanan-zaigo <asanashik7@gmail.com>
2021-05-17 15:56:26 +0530

    TextField component created as reusable component

--- Project_Reusable_Component/re_usable_component/src/App.js
+++ Project_Reusable_Component/re_usable_component/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import TextField from './components/TextField';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Form">
+        <form>
+          <TextField
+            value={name}
+            placeholder="Text"
+            label="Text Field"
+            name="name"
+            onChange={setName}
+          />
+        </form>
+      </div>
     </div>
   );
 }
